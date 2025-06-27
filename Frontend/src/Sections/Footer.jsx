6ee7a47f11ec25b96c1106d9ee0e0518a089c00e
Danish <danishshamshee123@gmail.com>
2025-06-27 23:34:28 +0530

    add profile picture on navbar

--- Frontend/src/Sections/Footer.jsx
+++ Frontend/src/Sections/Footer.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
     return (
         <footer className="w-full dark:bg-gray-900 bg-gray-100 dark:text-gray-400 text-gray-900 py-3 px-6 border-t dark:border-gray-800 border-gray-200">
-            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-8">
                 <div>
                     <h3 className="dark:text-gray-100 text-gray-900 text-lg font-bold mb-4 font-playfair">WOLVE<i className="text-2xl">N</i><span className="text-rose-600 dark:text-rose-500">STITCH</span></h3>
                     <p className="mb-4">Redefining luxury fashion with timeless elegance.</p>
@@ -24,6 +26,14 @@ export const Footer = () => {
                         <li><a href="#" className="hover:text-rose-400 transition">Contact Us</a></li>
                         <li><a href="#" className="hover:text-rose-400 transition">FAQs</a></li>
                         <li><a href="#" className="hover:text-rose-400 transition">Shipping</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 className="text-gray-100 font-semibold mb-4">Admin</h4>
+                    <ul className="space-y-2">
+                        <li><Link to="/add-product" className="hover:text-rose-400 transition">New Product</Link></li>
+                        <li><Link to="#" className="hover:text-rose-400 transition">Manage Products</Link></li>
+                        <li><Link to="#" className="hover:text-rose-400 transition">Manage Orders</Link></li>
                     </ul>
                 </div>
                 <div>
